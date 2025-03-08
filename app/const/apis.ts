6ef65dafd8240130344bd26a690d5ecb2c805ce3
wintun-devop <wintun.hlaing@outlook.com>
2025-03-08 14:51:29 +0630

    basic flow config

---
+++ app/const/apis.ts
@@ -0,0 +1,5 @@
+// Backend-api
+
+/* development */
+
+/* Production */
---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+export default function Home() {
+    return (
+        <>
+        <h5>Home Page</h5>
+        </>
+    )
+}
